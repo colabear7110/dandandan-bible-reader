8a23377e14d0bc580bed403e4be5c54bc6b56122
colabear7110 <craftsung@gmail.com>
2026-05-30 14:03:33 +0900

    Add new video entries for 6-2, 6-3, and 6-4

--- videos.js
+++ videos.js
@@ -151,5 +151,7 @@ window.VIDEO_BY_DATE = {
   "5-30": "ol3lT1ZmVK0",
   "5-31": "pXkzXwfmohA",
   "6-1": "4h_GJwoxlfE",
-  "6-2": "LOUf-oA2meg"
+  "6-2": "LOUf-oA2meg",
+  "6-3": "1AbL0KzfAZs",
+  "6-4": "CmFPYxmDC1A"
 };
