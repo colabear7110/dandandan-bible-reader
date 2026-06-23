8b68a734da48505a0d8da940be5652a179894aea
colabear7110 <craftsung@gmail.com>
2026-06-23 09:38:28 +0900

    Add entries for videos 6-24 and 6-25

--- videos.js
+++ videos.js
@@ -172,5 +172,7 @@ window.VIDEO_BY_DATE = {
   "6-20": "eS-VG0KRt7Q",
   "6-21": "o9GDrPYP37A",
   "6-22": "sHV3YlSCi6o",
-  "6-23": "GrlqnBKK7OQ"
+  "6-23": "GrlqnBKK7OQ",
+  "6-24": "kthlnZYKl04",
+  "6-25": "TNBCcgaoIqo"
 };
