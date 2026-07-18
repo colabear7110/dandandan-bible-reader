3a1e1469951369c76bb1c52a85e89fc5aad67454
colabear7110 <craftsung@gmail.com>
2026-07-18 15:49:43 +0900

    Add new video entries for 7-21 and 7-22

--- videos.js
+++ videos.js
@@ -199,5 +199,7 @@ window.VIDEO_BY_DATE = {
   "7-17": "b8_1tOIWKsI",
   "7-18": "MuDGPFNouvA",
   "7-19": "_YOXp2_Ju7s",
-  "7-20": "Ls8FpUceKyw"
+  "7-20": "Ls8FpUceKyw",
+  "7-21": "vIsNA9ndOEA",
+  "7-22": "sI7rkeibhdg"
 };
